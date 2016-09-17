6e15866f4f2cafc6c9cd30312a3b7da4ba4db9c7
mayank2k16 <mayankoli96@gmail.com>
2016-09-18 01:52:09 +0530

    nnn

---
+++ config/development/stripe.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'secret-key'=>'sk_test_dDGBpuPYAnVqIr4w3k2DlOJg',
+    'publishable-key'=>'pk_test_m9pwIfwnJ3cbxf7gQHjihWMu'
+];
---
+++ config/stripe.php
@@ -0,0 +1,6 @@
+<?php
+
+return[
+     'secret-key'=>'sk_live_SAzo3SRkmc7mn3vh3hskMTlV',  
+    'publishable-key'=>'pk_live_h9fpXwkdUlDEdNwFbQZ1UOUm'
+];
